34292bd902d7d13bdfa375d492f8c85c35f32a75
Anastasiia Erokhina <erokhinaanastasiia@gmail.com>
2021-12-17 16:44:32 +0200

    added Main Screen

---
+++ src/components/main-screen/main-screen.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainScreen = () => {
+  return (
+    <div className="container">
+      <h1>Main Screen</h1>
+    </div>
+  );
+};
+
+export default MainScreen;
