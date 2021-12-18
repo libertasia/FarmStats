dbad8e8c252de33151939c3139656a0faa50106c
Anastasiia Erokhina <erokhinaanastasiia@gmail.com>
2021-12-18 18:42:26 +0200

    Added provider and store

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import {BrowserRouter} from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+import {BrowserRouter} from "react-router-dom";
 import browserHistory from "./browser-history";
+import {composeWithDevTools} from "redux-devtools-extension";
+import App from './App';
+import rootReducer from "./store/root-reducer";
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools()
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter history={browserHistory}>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter history={browserHistory}>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
