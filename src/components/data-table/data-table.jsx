28dc027484e8e79495f40e41d1b65ae1da54e428
Anastasiia Erokhina <erokhinaanastasiia@gmail.com>
2021-12-21 16:57:58 +0200

    Added Pagination to DataTable component and mock data

--- src/components/data-table/data-table.jsx
+++ src/components/data-table/data-table.jsx
@@ -1,35 +1,59 @@
-import React from "react";
+import React, {useState} from "react";
 import {TableHeaders} from "../../const";
+import TablePagination from "@mui/material/TablePagination";
 
 const DataTable = (props) => {
   const {data} = props;
-return (
-    <table role="table" width="100%">
-      <caption className="visually-hidden">Farms Data</caption>
-      <thead>
-        <tr>
+
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  return (
+    <div>
+      <table role="table" width="100%">
+        <caption className="visually-hidden">Farms Data</caption>
+        <thead>
+          <tr>
+            {
+              TableHeaders.map ((item) =>
+              (
+                <th key={item.title}>{item.title}</th>
+              ))
+            }
+          </tr>
+        </thead>
+        <tbody>
           {
-            TableHeaders.map ((item) =>
-            (
-              <th key={item.title}>{item.title}</th>
-            ))
+             data.slice(page*rowsPerPage, page*rowsPerPage + rowsPerPage).map((entry, index) =>
+              (
+                <tr key={`entry-${entry[`location`]}-${index}`}>
+                  <td>{entry[`location`]}</td>
+                  <td>{entry[`datetime`]}</td>
+                  <td>{entry[`sensor_type`]}</td>
+                  <td>{entry[`value`]}</td>
+                </tr>
+              ))
           }
-        </tr>
-      </thead>
-      <tbody>
-      {
-        data.map((entry, index) =>
-          (
-            <tr key={`entry-${entry[`location`]}-${index}`}>
-              <td>{entry[`location`]}</td>
-              <td>{entry[`datetime`]}</td>
-              <td>{entry[`sensor_type`]}</td>
-              <td>{entry[`value`]}</td>
-            </tr>
-          ))
-      }
-      </tbody>
-    </table>
+        </tbody>
+      </table>
+      <TablePagination
+        component="div"
+        count={data.length}
+        page={page}
+        onPageChange={handleChangePage}
+        rowsPerPage={rowsPerPage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </div>
   );
 };
 
--- src/components/main-screen/main-screen.jsx
+++ src/components/main-screen/main-screen.jsx
@@ -11,97 +11,16 @@ const data = [
     "sensor_type": "temperature",
     "value": 1,
   },
-  {
-    "location": "farm1",
-    "datetime": "2021",
-    "sensor_type": "temperature",
-    "value": 2,
-  },
-  {
-    "location": "farm1",
-    "datetime": "2021",
-    "sensor_type": "temperature",
-    "value": 3,
-  },
-  {
-    "location": "farm1",
-    "datetime": "2021",
-    "sensor_type": "temperature",
-    "value": 4,
-  },
-  {
-    "location": "farm1",
-    "datetime": "2021",
-    "sensor_type": "temperature",
-    "value": 5,
-  },
-  {
-    "location": "farm1",
-    "datetime": "2021",
-    "sensor_type": "temperature",
-    "value": 6,
-  },
-  {
-    "location": "farm1",
-    "datetime": "2021",
-    "sensor_type": "temperature",
-    "value": 7,
-  },
-  {
-    "location": "farm1",
-    "datetime": "2021",
-    "sensor_type": "temperature",
-    "value": 8,
-  },
-  {
-    "location": "farm1",
-    "datetime": "2021",
-    "sensor_type": "temperature",
-    "value": 9,
-  },
-  {
-    "location": "farm1",
-    "datetime": "2021",
-    "sensor_type": "temperature",
-    "value": 10,
-  },
-  {
-    "location": "farm1",
-    "datetime": "2021",
-    "sensor_type": "temperature",
-    "value": 11,
-  },
-  {
-    "location": "farm1",
-    "datetime": "2021",
-    "sensor_type": "temperature",
-    "value": 12,
-  },
-  {
-    "location": "farm1",
-    "datetime": "2021",
-    "sensor_type": "temperature",
-    "value": 13,
-  },
-  {
-    "location": "farm1",
-    "datetime": "2021",
-    "sensor_type": "temperature",
-    "value": 14,
-  },
-  {
-    "location": "farm1",
-    "datetime": "2021",
-    "sensor_type": "temperature",
-    "value": 15,
-  },
-  {
-    "location": "farm1",
-    "datetime": "2021",
-    "sensor_type": "temperature",
-    "value": 16,
-  },
 ];
+
+for (let i = 2; i < 240; i++) {
+  data.push({
+    "location": "farm1",
+    "datetime": "2021",
+    "sensor_type": "temperature",
+    "value": i,
+  })
+}
 
 const MainScreen = () => {
   return (
