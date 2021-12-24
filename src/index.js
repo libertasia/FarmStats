84197670a480921cae620d8cce0d0bc0c6445c6b
Anastasiia Erokhina <erokhinaanastasiia@gmail.com>
2021-12-24 12:30:33 +0200

    created api

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {Provider} from "react-redux";
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
+import thunk from "redux-thunk";
 import {BrowserRouter} from "react-router-dom";
 import browserHistory from "./browser-history";
 import {composeWithDevTools} from "redux-devtools-extension";
 import App from './App';
 import rootReducer from "./store/root-reducer";
+import {createAPI} from "./services/api";
+
+const api = createAPI();
 
 const store = createStore(
   rootReducer,
-  composeWithDevTools()
+  composeWithDevTools(
+    applyMiddleware(thunk.withExtraArgument(api))
+  )
 );
 
 ReactDOM.render(
