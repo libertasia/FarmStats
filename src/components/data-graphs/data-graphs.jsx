631d5cb0f83b848d46b2a7bcef17b738c5727190
Anastasiia Erokhina <erokhinaanastasiia@gmail.com>
2021-12-28 13:32:22 +0200

    DataGraphs component done

--- src/components/data-graphs/data-graphs.jsx
+++ src/components/data-graphs/data-graphs.jsx
@@ -1,7 +1,7 @@
 import React from "react";
+import PropTypes from "prop-types";
 import AggregationSelectorForm from "../aggregation-selector-form/aggregation-selector-form";
-// import {connect} from "react-redux";
-
+import {connect} from "react-redux";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -11,7 +11,8 @@ import {
   Tooltip,
   Legend,
 } from 'chart.js';
-import { Bar } from 'react-chartjs-2';
+import {Bar} from 'react-chartjs-2';
+import {getActiveAggregation, getMonthlyStats, getSelectedLocations, getSelectedMetrics} from "../../store/selectors";
 
 ChartJS.register(
   CategoryScale,
@@ -22,62 +23,103 @@ ChartJS.register(
   Legend
 );
 
-const DataGraphs = (props) => {
-  // const {activeTab, isAllFarmsStatsLoaded, isAllFarmStatsLoading, isAllFarmsLoaded, loadFarmsStats, filteredStats} = props;
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * max);
+};
 
-  const options = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'top',
+const DataGraphs = (props) => {
+  const {activeAggregation, selectedLocations, selectedMetrics, monthlyStats} = props;
+
+  const graphColors = [];
+    selectedLocations.forEach(() => {
+      graphColors.push(`rgba(${getRandomInt(255)}, ${getRandomInt(255)}, ${getRandomInt(255)}, 0.5)`);
+    });
+
+  const generateGraphOptions = (metric) => {
+    const options = {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'top',
+        },
+        title: {
+          display: true,
+          text: metric.type,
+        },
       },
-      title: {
-        display: true,
-        text: 'Chart.js Bar Chart',
-      },
-    },
+    };
+    return options;
   };
 
-  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  const generateGraphData = (sensorType) => {
+    // sensorData has measurements for a sensor for each farm
+    const sensorData = monthlyStats.filter((element) => element.sensor_type === sensorType.type);
+    let groupedDataMap = new Map();
+    sensorData.forEach((element) => {
+      element.stats.forEach((statsElement) => {
+        const mapKey = `${statsElement.year}-${statsElement.month.toString().padStart(2, '0')}`;
+        if (!groupedDataMap.has(mapKey)) {
+          groupedDataMap.set(mapKey, [{farm_id: element.farm_id, value: statsElement[activeAggregation]}]);
+        } else {
+          let mapData = groupedDataMap.get(mapKey);
+          mapData.push({farm_id: element.farm_id, value: statsElement[activeAggregation]})
+          groupedDataMap.set(mapKey, mapData)
+        }
+      });
+    });
 
-  const data = {
-    labels,
-    datasets: [
-      {
-        label: 'Dataset 1',
-        data: [1, 2, 3, 4, 5],
-        backgroundColor: 'rgba(255, 99, 132, 0.5)',
-      },
-      {
-        label: 'Dataset 2',
-        data: [3, 4, 5, 6, 7],
-        backgroundColor: 'rgba(53, 162, 235, 0.5)',
-      },
-    ],
+    groupedDataMap = new Map([...groupedDataMap.entries()].sort())
+    const labels = [...groupedDataMap.keys()];
+    const datasets = [];
+    selectedLocations.forEach((location, index) => {
+      const data = [];
+      groupedDataMap.forEach((mapEntry) => {
+        let farmValueObj = mapEntry.find((e) => e.farm_id === location.farm_id)
+        if (farmValueObj) {
+          data.push(farmValueObj.value)
+        } else {
+          data.push(null)
+        }
+      })
+      datasets.push({
+        label: location.name,
+        data,
+        backgroundColor: graphColors[index],
+      });
+    });
+
+    const data = {
+      labels,
+      datasets,
+    };
+    return data;
   };
 
   return (
     <React.Fragment>
       <AggregationSelectorForm />
-      <Bar
-        options={options}
-        data={data}
-      />
+      {
+        selectedMetrics.map((metric) => (
+          <Bar key={metric.type}
+            options={generateGraphOptions(metric)}
+            data={generateGraphData(metric)}
+          />
+        ))
+      }
     </React.Fragment>
   );
 };
 
-// MainScreen.propTypes = {
-//   allFarmsStats: PropTypes.array,
-// };
+DataGraphs.propTypes = {
+  activeAggregation: PropTypes.string.isRequired,
+};
 
-// const mapStateToProps = (state) => ({
-//   activeTab: getActiveTab(state),
-//   filteredStats: getFilteredFarmsStats(state),
-//   isAllFarmsStatsLoaded: getAllFarmsStatsLoadedStatus(state),
-//   isAllFarmStatsLoading: getAllFarmsStatsLoadingStatus(state),
-//   isAllFarmsLoaded: getAllFarmsLoadedStatus(state),
-// });
+const mapStateToProps = (state) => ({
+  activeAggregation: getActiveAggregation(state),
+  selectedLocations: getSelectedLocations(state),
+  selectedMetrics: getSelectedMetrics(state),
+  monthlyStats: getMonthlyStats(state),
+});
 
 // const mapDispatchToProps = (dispatch) => ({
 //   loadFarmsStats() {
@@ -86,6 +128,5 @@ const DataGraphs = (props) => {
 //   },
 // });
 
-// export {DataGraphs};
-// export default connect(mapStateToProps, mapDispatchToProps)(DataGraphs);
-export default DataGraphs;
+export {DataGraphs};
+export default connect(mapStateToProps, null)(DataGraphs);
--- src/store/selectors.js
+++ src/store/selectors.js
@@ -8,6 +8,7 @@ export const getLocations = (state) => state[NameSpace.VIEW].locations;
 export const getActiveAggregation = (state) => state[NameSpace.VIEW].activeAggregation;
 export const getAllFarms = (state) => state[NameSpace.DATA].farms;
 export const getAllFarmsStats = (state) => state[NameSpace.DATA].farmsStats;
+export const getMonthlyStats = (state) => state[NameSpace.DATA].monthlyStats;
 export const getAllFarmsLoadedStatus = (state) => state[NameSpace.DATA].isAllFarmsLoaded;
 export const getAllFarmsStatsLoadedStatus = (state) => state[NameSpace.DATA].isAllFarmsStatsLoaded;
 export const getAllFarmsStatsLoadingStatus = (state) => state[NameSpace.DATA].isAllFarmsStatsLoading;
@@ -16,5 +17,19 @@ export const getFilteredFarmsStats = createSelector(
   [getAllFarmsStats, getLocations, getMetricTypes],
   (allData, locations, metrics) => {
     return generateFilteredData(allData, locations, metrics);
+  }
+);
+
+export const getSelectedLocations = createSelector(
+  [getLocations],
+  (locations) => {
+    return locations.filter((location) => location.isSelected);
+  }
+);
+
+export const getSelectedMetrics = createSelector(
+  [getMetricTypes],
+  (metrics) => {
+    return metrics.filter((metric) => metric.isSelected);
   }
 );
