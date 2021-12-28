41f7e8140129a9a7ffb9589d66213d852cb0d177
Anastasiia Erokhina <erokhinaanastasiia@gmail.com>
2021-12-28 16:27:23 +0200

    Added fonts

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <header className="wrapper__header container">
-      <h1>Header</h1>
+      <p>Solita Farms Statistics</p>
     </header>
   );
 };
