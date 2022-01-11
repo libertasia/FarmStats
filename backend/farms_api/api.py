f6fd10ce44d0515fc49299b868025b0a3f45496e
Anastasiia Erokhina <erokhinaanastasiia@gmail.com>
2022-01-11 19:04:35 +0200

    Working on aggregations

--- backend/farms_api/api.py
+++ backend/farms_api/api.py
@@ -1,5 +1,6 @@
 import csv
 import glob
+from os import stat
 from fastapi import FastAPI
 from farms_api.datastore import InMemoryDataStore
 
@@ -15,15 +16,34 @@ async def init_api():
     """
     Loads data from CSV files into datastore. Also adds some additional info about the farms.
     """
-    print("initting")
     data_directory = "./csv_data"
     csv_files = glob.glob(f"{data_directory}/*.csv")
-    print(csv_files)
     for file_path in csv_files:
         with open(file_path) as csv_file:
             csv_reader = csv.DictReader(csv_file)
             for line in csv_reader:
                 storage.add_stat_data(line)
+    # add some info about the farms
+    storage.add_farm_info({
+        "name": "Friman Metsola collective",
+        "location": "Metsola",
+        "established": "2017-11-08T01:48:57.669Z"
+    })
+    storage.add_farm_info({
+        "name": "PartialTech Research Farm",
+        "location": "Viikki",
+        "established": "2016-11-08T01:48:57.669Z"
+    })
+    storage.add_farm_info({
+        "name": "Noora's farm",
+        "location": "Helsinki",
+        "established": "2018-11-08T01:48:57.669Z"
+    })
+    storage.add_farm_info({
+        "name": "Organic Ossi's Impact That Lasts plantase",
+        "location": "Helsinki",
+        "established": "2021-11-08T01:48:57.669Z"
+    })
     return {}
 
 @app.get("/v1/farms")
@@ -31,25 +51,59 @@ async def get_all_farms():
     """
     Returns info about all farms.
     """
-    return {}
+    farms = storage.get_farms()
+    res = []
+    for farm in farms:
+        res.append({
+            "id": farm.id,
+            "name": farm.name,
+            "location": farm.location
+        })
+    return res
 
 @app.get("/v1/farms/{farm_id}")
-async def get_farm_details():
+async def get_farm_details(farm_id: int):
     """
     Returns info about a specific farm.
     """
-    return {}
+    farm = storage.get_farm_by_id(farm_id)
+    if not farm:
+        return {}
+    else:
+        return {
+            "id": farm.id,
+            "name": farm.name,
+            "location": farm.location,
+            "established": farm.established
+        }
 
 @app.get("/v1/farms/{farm_id}/stats")
-async def get_all_farm_stats():
+async def get_all_farm_stats(farm_id: int):
     """
     Returns all stats for a farm.
     """
-    return {}
+    stats = storage.get_farm_stats(farm_id)
+    return {
+        "measurements": stats
+    }
 
 @app.get("/v1/farms/{farm_id}/stats/{sensor_type}/monthly")
-async def get_monthly_stats_by_farm_sensor():
+async def get_monthly_stats(farm_id: int, sensor_type: str):
     """
     Returns stats for a given sensor, for a given farm, aggregated monthly.
     """
-    return {}
+    stats = storage.get_monthly_stats(farm_id, sensor_type)
+    stat_list = []
+    for stat in stats:
+        stat_list.append({
+            "month": stat.month,
+            "year": stat.year,
+            "average": stat.average,
+            "median": stat.median,
+            "standard_deviation": stat.stddev
+        })
+    return {
+        "farm_id": farm_id,
+        "sensor_type": sensor_type,
+        "stats": stat_list
+    }
--- backend/farms_api/datastore.py
+++ backend/farms_api/datastore.py
@@ -1,10 +1,18 @@
 from datetime import datetime
-from typing import Dict, Optional
+from typing import Dict, List, Optional
+from statistics import median
 
 SENSOR_NAME_TEMP = "temperature"
 SENSOR_NAME_RAINFALL = "rainFall"
 SENSOR_NAME_PH = "pH"
 VALID_SENSORS = [SENSOR_NAME_TEMP, SENSOR_NAME_RAINFALL, SENSOR_NAME_PH]
+
+class Farm:
+    def __init__(self, name: str, location: str, established: datetime, id: Optional[str] = None) -> None:
+        self.id = id
+        self.name = name
+        self.location = location
+        self.established = established
 
 class Measurement:
     def __init__(self, farm_name: str, measure_date: datetime, sensor_type: str, value: float) -> None:
@@ -13,15 +21,27 @@ class Measurement:
         self.sensor_type = sensor_type
         self.value = value
 
+class MonthlyAggregation:
+    def __init__(self, month: int, year: int, average: float, median: float, stddev: float) -> None:
+        self.month = month
+        self.year = year
+        self.average = average
+        self.median = median
+        self.stddev = stddev
+
 class DataStore:
     """
     Generic DataStore class, child classes must implement specific methods by themselves.
     """
-    def __init__(self) -> None:
-        self.stats = []
-        self.farms = []
 
-    def parse_raw_data(self, data: Dict[str, str]) -> Optional[Measurement]:
+    def parse_raw_farm_data(self, data: Dict[str, str]) -> Optional[Farm]:
+        name = data.get("name")
+        location = data.get("location")
+        established = data.get("established")
+        established = datetime.strptime(established, "%Y-%m-%dT%H:%M:%S.%f%z")
+        return Farm(name, location, established)
+
+    def parse_raw_stats_data(self, data: Dict[str, str]) -> Optional[Measurement]:
         """
         Parses raw string data and returns a Measurement object.
         If the data doesn't pass validation, returns None.
@@ -53,17 +73,90 @@ class DataStore:
     def add_farm_info(self, data: Dict[str, str]) -> None:
         raise NotImplementedError
 
+    def get_farms(self) -> List[Farm]:
+        raise NotImplementedError
+
+    def get_farm_by_id(self, farm_id: int) -> Farm:
+        raise NotImplementedError
+
+    def get_farm_stats(self, farm_id: int) -> List[Measurement]:
+        raise NotImplementedError
+
+    def get_monthly_stats(self, farm_id: int, sensor_type: str) -> List[MonthlyAggregation]:
+        raise NotImplementedError
 
 class InMemoryDataStore(DataStore):
     """
     DataStore which keeps all the data in memory.
     """
+    stats: List[Measurement]
+    farms: List[Farm]
+
+    def __init__(self) -> None:
+        self.stats = []
+        self.farms = []
+
     def add_stat_data(self, data: Dict[str, str]) -> None:
-        print(f"parsing {data}")
-        parsed_data = self.parse_raw_data(data)
+        parsed_data = self.parse_raw_stats_data(data)
         if parsed_data:
-            print("done")
             self.stats.append(parsed_data)
 
     def add_farm_info(self, data: Dict[str, str]) -> None:
-        self.farms.append(data)
+        parsed_data = self.parse_raw_farm_data(data)
+        if parsed_data:
+            if not parsed_data.id:
+                existing_ids = [int(x.id) for x in self.farms]
+                if len(existing_ids) > 0:
+                    new_id = max(existing_ids) + 1
+                else:
+                    new_id = 1
+                parsed_data.id = new_id
+            self.farms.append(parsed_data)
+
+    def get_farms(self) -> List[Farm]:
+        return self.farms
+
+    def get_farm_by_id(self, farm_id) -> Farm:
+        for farm in self.farms:
+            if farm.id == farm_id:
+                return farm
+        return None
+
+    def get_farm_stats(self, farm_id) -> List[Measurement]:
+        res = []
+        farm = self.get_farm_by_id(farm_id)
+        print(farm.name)
+        if not farm:
+            return res
+        for measurement in self.stats:
+            if measurement.farm_name == farm.name:
+                res.append(measurement)
+        return res
+
+    def get_monthly_stats(self, farm_id: int, sensor_type: str) -> List[MonthlyAggregation]:
+        res = []
+        farm = self.get_farm_by_id(farm_id)
+        if not farm:
+            return res
+        farm_measurements = [m for m in self.stats if m.farm_name == farm.name]
+        # this dictionary will have yyyy-mm as keys and measurements as values
+        monthly_data = {}
+        for measurement in farm_measurements:
+            key = f"{measurement.date.year}-{measurement.date.month}"
+            if not key in monthly_data:
+                monthly_data[key] = {
+                    "month": measurement.date.month,
+                    "year": measurement.date.year,
+                    "values": []
+                }
+            monthly_data[key]["values"].append(measurement.value)
+        print(monthly_data)
+        for key, value in monthly_data.items():
+            res.append(MonthlyAggregation(
+                value["month"],
+                value["year"],
+                sum(value["values"]) / len(value["values"]),
+                0,
+                0
+            ))
+        return res
