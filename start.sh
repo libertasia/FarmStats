6671428ee2e6bce18009a99e33facdb89539a53e
Anastasiia Erokhina <erokhinaanastasiia@gmail.com>
2022-01-15 14:49:01 +0200

    Add delay to start script

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 docker-compose up --build --detach
+# allow all service to come up
+sleep 5
+echo "Loading initial data..."
 # load initial dataset
 curl -XPOST http://localhost:8080/init
 echo ""
